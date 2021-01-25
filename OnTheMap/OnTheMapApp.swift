9e85df68d73bb3937769c2e904c3da8deef6f855
Ataias Pereira Reis <ataiasreis@gmail.com>
2021-01-25 06:35:23 -0300

    Initial Commit

---
+++ OnTheMap/OnTheMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTheMapApp.swift
+//  OnTheMap
+//
+//  Created by Ataias Pereira Reis on 25/01/21.
+//
+
+import SwiftUI
+
+@main
+struct OnTheMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
